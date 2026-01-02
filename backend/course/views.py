313ea533d186655f6cd94c8db9d918e226bb6686
Ripon <ripon1995dc@gmail.com>
2026-01-02 16:38:49 +0600

    pre-hook

--- backend/course/views.py
+++ backend/course/views.py
@@ -3,13 +3,6 @@ from rest_framework.permissions import AllowAny
 
 from .models import Course
 from .serializers import CourseCreateSerializer
-
-
-
-
-
-
-
 class CourseListCreateAPIView(ListCreateAPIView):
     permission_classes = [AllowAny]
     queryset = Course.objects.all()
