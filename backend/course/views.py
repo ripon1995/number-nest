70184836fe442cc59e588a0b18f3f368ee696033
rootless <ripon1995dc@gmail.com>
2026-01-19 03:12:51 +0600

    removed view wrapper from this views

--- backend/course/views.py
+++ backend/course/views.py
@@ -1,10 +1,17 @@
 from rest_framework.generics import ListCreateAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
+
+from utils import success_response
 from .models import Course
 from .serializers import CourseCreateSerializer
 
 
 class CourseListCreateAPIView(ListCreateAPIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     queryset = Course.objects.all()
     serializer_class = CourseCreateSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.queryset
+        serializer = self.get_serializer(queryset, many=True)
+        return success_response(data=serializer.data, message="Course list fetched successfully")
