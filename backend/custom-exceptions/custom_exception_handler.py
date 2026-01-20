d0f083bb040d913371b5293178d9eed2c7191038
Ripon <ripon1995dc@gmail.com>
2026-01-20 11:25:11 +0600

    3-authentication: custom exception handler improved

--- backend/custom-exceptions/custom_exception_handler.py
+++ backend/custom-exceptions/custom_exception_handler.py
@@ -6,23 +6,48 @@ from utils.error_response import error_response
 logger = logging.getLogger(__name__)
 
 
+def flatten_errors(error_dict):
+    """
+    Converts {'field': ['Error message']} into 'field: Error message'
+    """
+    messages = []
+    for field, errors in error_dict.items():
+        if isinstance(errors, list):
+            # Take the first error for that field
+            messages.append(f"{field.replace('_', ' ').capitalize()}: {errors[0]}")
+        else:
+            messages.append(f"{field}: {errors}")
+    return " ".join(messages)
+
+
+def __handle_client_error(response):
+    errors = flatten_errors(response.data)
+    message = "Bad Request"
+    match response.status_code:
+        case status.HTTP_400_BAD_REQUEST:
+            message = "Bad Request"
+        case status.HTTP_401_UNAUTHORIZED:
+            message = "Unauthorized"
+
+    return message, errors
+
+
 def custom_exception_handler(exc, context):
-    # logger.error(exc, exc_info=True)
-    logger.debug(f"Caught an error: {type(exc).__name__}")
-    logger.debug(f"Occurred in view: {context['view'].__class__.__name__}")
-    # drf exception handler to get the standard exception
     response = exception_handler(exc, context)
 
-    if response.status_code==401:
+    message = "An unknown error occurred."
+
+    if status.is_client_error(response.status_code):
+        message, errors = __handle_client_error(response)
         return error_response(
-            message=response.data['detail'],
-            errors=response.status_text,
+            message=message,
+            errors=response.data,
             status_code=response.status_code,
         )
 
     logger.critical(f"Unhandled Exception: {exc}", exc_info=True)
     return error_response(
-        message='Internal Server Error',
-        errors=exc.errors() if hasattr(exc, 'errors') else None,
+        message=message,
+        errors=exc.errors() if hasattr(exc, "errors") else None,
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
     )
