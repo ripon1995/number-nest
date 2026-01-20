5380c9bd114d24d687891f57344ba81973f0b86c
Ripon <ripon1995dc@gmail.com>
2026-01-20 13:09:15 +0600

    3-authentication: validation added in course creation

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,47 +11,42 @@ start_mongodb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'corsheaders',
-    'course',
-    'user'
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "corsheaders",
+    "course",
+    "user",
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 REST_FRAMEWORK = {
-    'EXCEPTION_HANDLER': 'custom-exceptions.custom_exception_handler.custom_exception_handler',
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'user.authentication.MongoJWTAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    )
+    "EXCEPTION_HANDLER": "custom_exceptions.custom_exception_handler.custom_exception_handler",
+    "DEFAULT_AUTHENTICATION_CLASSES": ("user.authentication.MongoJWTAuthentication",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=10),
-    'ROTATE_REFRESH_TOKENS': True,  # important → issues new refresh on each refresh
-    'BLACKLIST_AFTER_ROTATION': True,  # revoke old refresh after rotation
-    'UPDATE_LAST_LOGIN': True,
-
-    'ALGORITHM': 'HS256',
-    'SIGNING_KEY': SECRET_KEY,
-    'AUTH_HEADER_TYPES': ('Bearer',),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ROTATE_REFRESH_TOKENS": True,  # important → issues new refresh on each refresh
+    "BLACKLIST_AFTER_ROTATION": True,  # revoke old refresh after rotation
+    "UPDATE_LAST_LOGIN": True,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
 # CORS settings
@@ -60,16 +55,16 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = "config.urls"
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -78,9 +73,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- backend/course/serializers.py
+++ backend/course/serializers.py
@@ -1,6 +1,12 @@
-from rest_framework.serializers import Serializer, IntegerField, CharField, DateTimeField, DecimalField
-
+from rest_framework.serializers import (
+    Serializer,
+    IntegerField,
+    CharField,
+    DateTimeField,
+    DecimalField,
+)
 from .models import Course
+from custom_exceptions import CourseCapacityFullException
 
 
 class CourseCreateSerializer(Serializer):
@@ -11,6 +17,11 @@ class CourseCreateSerializer(Serializer):
     batch_time = DateTimeField(required=True)
     capacity = IntegerField(required=True)
     course_fee = DecimalField(required=True, max_digits=4, decimal_places=0)
+
+    @staticmethod
+    def validate_capacity(capacity):
+        if capacity <= 0:
+            raise CourseCapacityFullException()
 
     def create(self, validated_data):
         course = Course(**validated_data)
