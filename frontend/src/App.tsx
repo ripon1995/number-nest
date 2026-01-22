c8a3c67fa36eaa08e8c7e1ada39ab1e796e13850
Ripon <ripon1995dc@gmail.com>
2026-01-22 15:05:44 +0600

    FE-add-menue: route added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,10 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Registration from './components/Registration';
+import EnrolledStudents from './components/EnrolledStudents.tsx';
 import Login from './components/Login';
 import {AppRoutes} from "./constants/appRoutes.ts";
+import CourseDetail from "./components/CourseDetail.tsx";
 
 function App() {
     return (
@@ -12,6 +14,8 @@ function App() {
                 <Route path={AppRoutes.LOGIN} element={<Login/>}/>
                 <Route path={AppRoutes.REGISTER} element={<Registration/>}/>
                 <Route path={AppRoutes.DASHBOARD} element={<Dashboard/>}/>
+                <Route path={AppRoutes.COURSE_DETAILS} element={<CourseDetail/>}/>
+                <Route path={AppRoutes.ENROLLED_STUDENTS} element={<EnrolledStudents/>}/>
             </Routes>
         </BrowserRouter>
     );
--- frontend/src/constants/appRoutes.ts
+++ frontend/src/constants/appRoutes.ts
@@ -2,4 +2,8 @@ export const AppRoutes = {
     LOGIN: '/',
     REGISTER: '/register',
     DASHBOARD: '/dashboard',
+    ENROLLED_STUDENTS: '/enrolled-students',
+    COURSE_DETAILS: '/course/:id',
+    // Helper to generate the URL
+    getCoursePath: (id: string) => '/course/' + id
 } as const;
