61476ec58c2488903d89163a3b2faa2d19f66363
rootless <ripon1995dc@gmail.com>
2026-01-23 02:50:17 +0600

    FE-add-menu: menues added

--- frontend/src/components/Course-components/QuickActionSection.tsx
+++ frontend/src/components/Course-components/QuickActionSection.tsx
@@ -1,4 +1,11 @@
-import {IoPersonOutline, IoSettingsOutline, IoHelpCircleOutline} from 'react-icons/io5';
+import {IoPersonOutline} from 'react-icons/io5';
+import {BiTask} from "react-icons/bi";
+import {PiStudentBold} from "react-icons/pi";
+import {SiWikibooks} from "react-icons/si";
+import {RiCalendarScheduleLine} from "react-icons/ri";
+import {TfiWrite} from "react-icons/tfi";
+import {RiMoneyDollarCircleLine} from "react-icons/ri";
+import {LiaBookSolid} from "react-icons/lia";
 import MenuCard from '../MenuCard';
 import {AppRoutes} from '../../constants/appRoutes';
 
@@ -8,27 +15,58 @@ interface QuickActionsSectionProps {
 
 export const QuickActionsSection = ({onNavigate}: QuickActionsSectionProps) => (
     <section className="quick-actions">
-        <hr className="section-divider"/>
-        <h2 className="section-title">Quick Actions</h2>
         <div className="menu-grid">
+            <MenuCard
+                title="Course Plan"
+                description="Detailed roadmap."
+                icon={<SiWikibooks/>}
+                onClick={() => onNavigate('/course-plan')}
+            />
+            <MenuCard
+                title="Course Routine"
+                description="Weekly class timetable."
+                icon={<RiCalendarScheduleLine/>}
+                onClick={() => onNavigate('/course-routine')}
+            />
+            <MenuCard
+                title="Enrolled Students"
+                description="Complete list of participants."
+                icon={<PiStudentBold/>}
+                onClick={() => onNavigate('/enrolled-students')}
+            />
+            <MenuCard
+                title="Attendance"
+                description="Track daily presence."
+                icon={<BiTask/>}
+                onClick={() => onNavigate('/course-attendance')}
+            />
+            <MenuCard
+                title="Exam Schedule"
+                description="Upcoming test dates."
+                icon={<TfiWrite/>}
+                onClick={() => onNavigate('/exam-schedule')}
+            />
+            <MenuCard
+                title="Payment"
+                description="Collected payment."
+                icon={<RiMoneyDollarCircleLine/>}
+                onClick={() => onNavigate('/course-payment')}
+            />
+            <MenuCard
+                title="Books & Docs"
+                description="Important notes."
+                icon={<LiaBookSolid/>}
+                onClick={() => onNavigate('/course-notes')}
+            />
+
+
             <MenuCard
                 title="My Profile"
                 description="View and edit your personal details"
                 icon={<IoPersonOutline/>}
                 onClick={() => onNavigate(AppRoutes.PROFILE)}
             />
-            <MenuCard
-                title="Settings"
-                description="Account and notification preferences"
-                icon={<IoSettingsOutline/>}
-                onClick={() => onNavigate('/settings')}
-            />
-            <MenuCard
-                title="Help Center"
-                description="FAQs and student support"
-                icon={<IoHelpCircleOutline/>}
-                onClick={() => onNavigate('/help')}
-            />
+
         </div>
     </section>
 );
