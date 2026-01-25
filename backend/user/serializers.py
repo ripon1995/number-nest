03ce18e5bb5a0df366b2f3a8f5359e08208db468
Ripon <ripon1995dc@gmail.com>
2026-01-25 20:42:54 +0600

    fe

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,6 +1,12 @@
-from rest_framework.serializers import Serializer, ValidationError, CharField
+from rest_framework.serializers import (
+    Serializer,
+    ValidationError,
+    CharField,
+    SerializerMethodField,
+)
 from django.contrib.auth.hashers import check_password
-from .models import User
+from .models import User, UserProfile
+from rest_framework_mongoengine import serializers as mongo_serializers
 
 
 class UserRegistrationSerializer(Serializer):
@@ -37,3 +43,37 @@ class UserLoginSerializer(Serializer):
 
         data["user"] = user
         return data
+
+
+class UserProfileSerializer(mongo_serializers.DocumentSerializer):
+    father_name = SerializerMethodField(allow_null=True)
+    college = SerializerMethodField(allow_null=True)
+    father_contact = SerializerMethodField(allow_null=True)
+    email = SerializerMethodField(allow_null=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "name",
+            "phone_number",
+            "father_name",
+            "college",
+            "father_contact",
+            "email",
+        ]
+
+    def get_father_name(self, obj):
+        profile = self.context.get("profile")
+        return profile.father_name if profile else None
+
+    def get_college(self, obj):
+        profile = self.context.get("profile")
+        return profile.college if profile else None
+
+    def get_father_contact(self, obj):
+        profile = self.context.get("profile")
+        return profile.father_contact if profile else None
+
+    def get_email(self, obj):
+        profile = self.context.get("profile")
+        return profile.email if profile else None
--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,8 +1,9 @@
 # backend/user/urls.py
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, ProfileView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("me/", ProfileView.as_view(), name="me"),
 ]
