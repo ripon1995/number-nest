0b8ff05526efd0966cbc9d09f2013894810e871d
rootless <ripon1995dc@gmail.com>
2026-02-02 16:39:03 +0600

    BE - update api

--- frontend/src/components/Course-components/CourseEnrolledStudentsView.tsx
+++ frontend/src/components/Course-components/CourseEnrolledStudentsView.tsx
@@ -1,30 +1,28 @@
 // CourseEnrolledStudentsView.tsx
 import {Paper, Typography} from '@mui/material';
+import {useParams} from 'react-router-dom'; // 1. Import useParams
 import StudentListComponent from '../student-components/student-list-component.tsx';
 import {useStudentStore} from "../../store/useStudentStore.ts";
 import {useEffect} from "react";
 
 export const CourseEnrolledStudentsView = () => {
-
-    const students = useStudentStore((state) => state.students);
-    const fetchStudents = useStudentStore((state) => state.fetchStudents);
+    const {id} = useParams<{ id: string }>();
+    const {students, fetchStudents} = useStudentStore();
 
     useEffect(() => {
         const controller = new AbortController();
-        const COURSE_ID = '697677e9de25671ccc2dd509';
 
         // Just pass the string directly as the first argument
-        fetchStudents(COURSE_ID, controller.signal);
+        fetchStudents(id, controller.signal);
 
         return () => controller.abort();
-    }, [fetchStudents]);
+    }, [fetchStudents, id]);
 
     return (
         <Paper className="view-container" elevation={2} sx={{p: 3}}>
             <Typography variant="h4" component="h1" gutterBottom>
                 Enrolled Students
             </Typography>
-
             <StudentListComponent students={students}></StudentListComponent>
         </Paper>
     );
